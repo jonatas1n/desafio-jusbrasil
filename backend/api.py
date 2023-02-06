0077a5aa3a793659fec34682985345173c78a11e
jonatas1n <jonatasgeek@gmail.com>
2023-02-05 21:11:58 -0300

    Set backend documentation

--- backend/api.py
+++ backend/api.py
@@ -1,13 +1,68 @@
 from fastapi import FastAPI
 from database import Database
 from fastapi_pagination import Page, add_pagination, paginate
-from models import SearchResponse, PostRequest, MovementResponse, FiltersRequest
+from models import SearchResponse, PostRequest, FiltersRequest
 import uvicorn
 
 from fastapi.middleware.cors import CORSMiddleware
 
 database = Database()
-app = FastAPI()
+
+description = '''
+**Probusca - Se mantenha atualizado sobre os processos que você tem interesse** 🔎
+----
+Essa API retorna dados sobre processos extraídos de diversos estados Brasileiros, entre várias comarcas e tribunais.
+
+Os dados persistidos aqui são de 11 estados:
+- Amapá *(TJPA)*
+- Amazonas *(TRF1)*
+- Ceará *(TJCE)*
+- Distrito Federal *(CNJ)*
+- Espírito Santo  *(TJES)*
+- Mato Grosso *(TJMT)*
+- Minas Gerais *(TJMG)*
+- Paraíba *(TJPB)*
+- Piauí *(TRF1)*
+- Tocantins *(TRF1)*
+'''
+
+tags_metadata = [
+    {
+        "name": "Processo",
+        "description": "Retorna dados sobre o processo do número indicado.",
+    },
+    {
+        "name": "Busca",
+        "description": "Retorna o resultado de uma busca por processos.",
+    },
+    {
+        "name": "Busca Filtrada",
+        "description": "Retorna o resultado de uma busca, mas com filtros nos parâmetros."
+    },
+    {
+        "name": "Participantes",
+        "description": "Retorna todos os participantes de determinado processo."
+    },
+    {
+        "name": "Movimentação",
+        "description": "Retorna a movimentação de determinado processo."
+    },
+    {
+        "name": "Filtros",
+        "description": "Retorna uma lista de filtros para utilização no frontend."
+    }
+]
+
+app = FastAPI(
+    title="Probusca API",
+    description=description,
+    version='2.1.0',
+    contact={
+        "name": "Jônatas Gomes Barbosa da Silva",
+        "email": "jonatasgomesb@gmail.com"
+    },
+    openapi_tags=tags_metadata
+)
 
 origins = ["http://localhost",]
 
@@ -19,28 +74,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/api/{code}")
+@app.get("/api/{code}", tags=["Processo"])
 async def process(code:str):
     process = database.get_process(code)
     if process is None:
         return "No process found"
     return process
 
-@app.get("/api/", response_model=Page[SearchResponse])
+@app.get("/api/", response_model=Page[SearchResponse], tags=["Busca"])
 async def search(search:str|None=None):
     if search is None:
         return "No parameters set"
     results = database.search(search)
     return paginate(results)
 
-@app.post("/api/",  response_model=Page[SearchResponse])
+@app.post("/api/",  response_model=Page[SearchResponse], tags=["Busca Filtrada"])
 async def filtered_search(filters: PostRequest,):
     filters = [tuple(filter) for filter in filters if filter[1] is not None]
     filters = dict(filters)
     results = database.filtered_search(**filters)
     return paginate(results)
 
-@app.get("/api/participants/{code}")
+@app.get("/api/participants/{code}", tags=["Participantes"])
 async def get_participants(code:str, type:str='active'):
     type_functions = {
         'active': database.get_active,
@@ -50,12 +105,12 @@ async def get_participants(code:str, type:str='active'):
     participants = type_functions[type](code)
     return participants
 
-@app.get('/api/movement/{code}')
+@app.get('/api/movement/{code}', tags=["Movimentação"])
 async def get_movement(code: str):
     result = database.get_movement(code)
     return result
 
-@app.post('/api/filters/')
+@app.post('/api/filters/', tags=["Filtros"])
 async def get_filter_list(filters: FiltersRequest):
     filters = [tuple(filter) for filter in filters if filter[1] is not None]
     filters = dict(filters)
