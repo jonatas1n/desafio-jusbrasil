84e14bdcc32ac7fbd3ba6b4e20f1e3d8d9c591a0
jonatas1n <jonatasgeek@gmail.com>
2023-02-05 21:20:36 -0300

    remove cypress

--- tests/cypress/e2e/processPage/notification.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Test notification icon', () => {
-    it('check if movements have items', () => {
-        cy.visit('/process?id=10066148920224014002');
-        cy.wait(800);
-        cy.get('.bell-button svg').should('have.css', 'color', 'rgb(216, 216, 216)');
-        cy.get('.bell-button').click();
-        cy.get('.bell-button svg').should('have.css', 'color', 'rgb(0, 0, 0)');
-    })
-});
